f232081e78669756d0ce3e02253876aac1785050
Nampheung1249 <89174607+Nampheung1249@users.noreply.github.com>
2021-11-15 14:38:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,9 +149,9 @@ def event_handle(event):
 
     if msgType == "text":
         msg = str(event["message"]["text"])
-        if (msg == "สวัสดี") :
+        if msg == "สวัสดี" :
             replyObj = TextSendMessage(text="ดีเจ้าค่ะ")
-        if msg == "ไปไหนมา"
+        if msg == "ไปไหนมา" :
             replyObj = TextSendMessage(text="ไปเที่ยวมาจ้า")
         line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
